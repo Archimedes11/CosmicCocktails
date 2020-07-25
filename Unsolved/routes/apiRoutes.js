d041e54cba83819dbcbfb47503255bf5edc6db94
Sandy Smith <toosandra@gmail.com>
2020-07-25 12:55:43 -0400

    Created 3rd party api code to pull in specific categories. Debugged for issues with delete button, and models and front end.

--- Unsolved/routes/apiRoutes.js
+++ Unsolved/routes/apiRoutes.js
@@ -3,22 +3,36 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all examples
   app.get("/api/drinks", function(req, res) {
-    db.drinks.findAll({}).then(function(dbDrinks) {
+    db.Drink.findAll({}).then(function(dbDrinks) {
       res.json(dbDrinks);
     });
   });
 
   // Create a new example
   app.post("/api/drinks", function(req, res) {
-    db.drinks.create(req.body).then(function(dbDrinks) {
+    db.Drink.create(req.body).then(function(dbDrinks) {
       res.json(dbDrinks);
     });
   });
+  
+ // POST route for saving a new todo. We can create a todo using the data on req.body
+ app.post("/api/drinks", function(req, res) {
+  var drinkItem = {
+    drink_name: req.body.drink_name,
+    complete: req.body.complete
+  }
+  db.Drink.create(drinkItem).then(function(result) {
+    return res.json(result);
+  });
+});
 
   // Delete an example by id
   app.delete("/api/drinks/:id", function(req, res) {
-    db.drinks.destroy({ where: { id: req.params.id } }).then(function(dbDrinks) {
+    db.Drink.destroy({ where: { id: req.params.id } }).then(function(dbDrinks) {
       res.json(dbDrinks);
     });
   });
 };
+// PUT route for updating todos. We can access the updated todo in req.body
+//app.put("/api/drinks", function(req, res) {
+//});
