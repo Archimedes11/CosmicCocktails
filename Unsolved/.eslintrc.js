ed67901b7a157d15d8e47fd7f9f62d4cdaec5d42
Sandy Smith <toosandra@gmail.com>
2020-08-04 09:56:12 -0400

    added eslintrc.js

---
+++ Unsolved/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": false,
+        "es6": false
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2015
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
