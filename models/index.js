0e71cc9fe2fe947f2b332f9e9e9597b6f537e8e8
Adam Wolff <adam@adamkwolff.com>
2025-03-16 01:42:19 -0500

    Fixed MySQL connection and local setup

--- models/index.js
+++ models/index.js
@@ -1,40 +1,52 @@
 "use strict";
 
-var fs = require("fs");
-var path = require("path");
-var Sequelize = require("sequelize");
-var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.json")[env];
-var db = {};
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+require("dotenv").config(); // Load environment variables
 
-//var app = require("../server");
+const basename = path.basename(__filename);
+const db = {};
 
-
-//sequelize;
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
-
-fs.readdirSync(__dirname)
-  .filter(function(file) {
-    return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+// Correct JawsDB MySQL Connection for Heroku
+const sequelize = process.env.DB_HOST
+  ? new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
+        dialect: "mysql",
+        logging: false, // Disable SQL logging for production
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false, // Required for JawsDB SSL
+          },
+        },
+      }
+    )
+  : new Sequelize(
+      "drinks_db", // Local database
+      "root",
+      "Bornin1989.",
+      {
+        host: "localhost",
+        dialect: "mysql",
+        logging: false,
+      }
     );
-  })
-  .forEach(function(file) {
-    var model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+
+// Load models dynamically
+fs.readdirSync(__dirname)
+  .filter((file) => file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js")
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(function(modelName) {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
--- utilities-backend/cocktailapi.js
+++ utilities-backend/cocktailapi.js
@@ -1,11 +1,10 @@
-//This loads the enviroment variables from the .env folder
-//require("dotenv").config();
-//var axios = require("axios");
-//import axios from "axios";
 const axios = require('axios').default;
-//Import the keys.js file
-//var keys = require("/keys.js");
+
 var parseDrink = (data) => {
+    if (!data) {
+        return null;
+    }
+
     var drink = {
         drinkName: data.strDrink,
         alcohol: data.strAlcoholic,
@@ -14,50 +13,45 @@ var parseDrink = (data) => {
         drinkImage: data.strDrinkThumb,
         idDrink: data.idDrink,
         ingredients: []
-    }
+    };
 
+    for (var j = 1; j <= 15; j++) {
+        var name = data["strIngredient" + j];
+        var measurement = data["strMeasure" + j];
 
-    for(var j = 1; j < 16; j++) {
-        var name = data["strIngredient" + j]
-        var measurement = data["strMeasure" + j]
-
-        if(name !== null) {
-            drink.ingredients.push({name, measurement})
+        if (name !== null) {
+            drink.ingredients.push({ name, measurement });
         } else {
             break;
         }
     }
 
     return drink;
-}
-
-    
-   
+};
 
 var drinkUtilities = {
-    getDrinksFromCocktaildb: function(searchQuery, callback) {
-        // Run the get request via json using axios
+    getDrinksFromCocktaildb: function (searchQuery, callback) {
         axios.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + searchQuery)
             .then(function (response) {
-                var drinks = response.data.drinks;
-                var drinks_final = [];
-
-                // Process each drink data
-                for (var i = 0; i < drinks.length; i++) {
-                    var drink = parseDrink(drinks[i]);
-                    drinks_final.push(drink);
-                }
-
+                var drinks = response.data.drinks || []; // Fix: Handle null response
+                var drinks_final = drinks.map(parseDrink).filter(d => d !== null);
                 callback(drinks_final);
-            }
-        );
+            })
+            .catch(err => {
+                console.error("Error fetching drinks:", err);
+                callback([]); // Return empty array on error
+            });
     },
 
     getCocktailByID: async (idDrink) => {
-        const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`)
-        const drink_data = response.data.drinks[0];
-        return parseDrink(drink_data);
+        try {
+            const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`);
+            return parseDrink(response.data.drinks ? response.data.drinks[0] : null);
+        } catch (err) {
+            console.error("Error fetching cocktail:", err);
+            return null;
+        }
     }
-}
+};
 
 module.exports = drinkUtilities;
